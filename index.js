6a139d9d4526ca4a7165e017439125564076ffe1
Md Rayyan Sarfaraz <mdrayyansarfaraz@gmail.com>
2026-01-04 15:26:11 +0530

    bug fix

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(
-  cors({
-    origin: "http://localhost:3000", 
-    credentials: true,
-  })
+  cors()
 );
 
 app.use("/api", routes);
